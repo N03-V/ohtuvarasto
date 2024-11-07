182196334949f8981d52799b97d856521f2ab3ee
Viianen Noe <noe.n.viianen@student.jyu.fi>
2024-11-07 18:06:06 +0200

    Tehtävä 1

--- src/index.py
+++ src/index.py
@@ -1,19 +1,20 @@
 from varasto import Varasto
 
 
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def tulosta_luonnin_jalkeen(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+
+def tulosta_olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+
+def tulosta_mehu(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +23,8 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+
+def tulosta_virhe():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,27 +34,56 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+
+def tulosta_olutvarasto1(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+
+def tulosta_mehuvarasto1(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+
+def tulosta_olutvarasto2(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+
+def tulosta_mehuvarasto2(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    tulosta_luonnin_jalkeen(mehua, olutta)
+
+    tulosta_olut_getterit(olutta)
+
+    tulosta_mehu(mehua)
+
+    tulosta_virhe()
+
+    tulosta_olutvarasto1(olutta)
+
+    tulosta_mehuvarasto1(mehua)
+
+    tulosta_olutvarasto2(olutta)
+
+    tulosta_mehuvarasto2(mehua)
 
 
 if __name__ == "__main__":
